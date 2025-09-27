cdee84d287af299e743031cb57ae07321ba69890
redthecoderist1000 <redochavillo@gmail.com>
2025-09-27 12:07:34 +0800

    fix: document title

--- src/helper/DashboardLayout.jsx
+++ src/helper/DashboardLayout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "./NavBar"; // Make sure Sidebar path is correct
 
 const DashboardLayout = () => {
+  document.title = "NUD Assess";
   return (
     <div className="flex h-screen bg-white">
       {/* Sidebar (Fixed on the left) */}
